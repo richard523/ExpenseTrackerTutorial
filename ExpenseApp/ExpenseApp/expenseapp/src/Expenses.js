247ac0ee64f497dead882b34e79c7d6c12b1a2ac
richard523 <60868589+richard523@users.noreply.github.com>
2022-04-23 12:37:12 -0500

    Vulnerabilities fixed

--- ExpenseApp/ExpenseApp/expenseapp/src/Expenses.js
+++ ExpenseApp/ExpenseApp/expenseapp/src/Expenses.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import AppNav from './AppNav';
+import DatePicker from "react-datepicker";
 
 class Expenses extends Component {
     state = {  } 
     render() { 
         return ( 
-        <div>
-            <AppNav/>
-            <h2>Expenses</h2>
-        </div>
+            <div>
+                <AppNav/>
+                <h2>Expenses</h2>
+            </div>
         );
     }
 }
